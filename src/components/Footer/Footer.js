6096a84deb223115add9cfdad9eb8b1567f5de9c
arsalannury <arsiiiland@yaho.com>
2022-03-19 19:05:25 +0330

    footer styles

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -39,31 +39,38 @@ const Footer = () => {
           <LinkWrapper
             container
             alignItems={"center"}
-            justifyContent={"space-around"}
+            justifyContent={"space-between"}
             item
             xs={12}
             sm={12}
             md={6}
             lg={6}
           >
+            
+            <Flist item xs={12} sm={6} md={6} lg={4} xl={4}>
             <Ul>
               <LiTitle>راه های ارتباطی</LiTitle>
               <Li>مراجعه حضوری</Li>
               <Li>پست الکترونیک</Li>
             </Ul>
+            </Flist>
+            <Slist item xs={12} sm={6} md={6} lg={4} xl={4}>
             <Ul>
               <LiTitle>خدمات ما</LiTitle>
               <Li>ارسال رایگان تهران</Li>
               <Li>ارسال یک روزه شهرستان</Li>
             </Ul>
+            </Slist>
+            <Tlist item xs={12} sm={6} md={6} lg={4} xl={4}>
             <Ul>
               <LiTitle>نحوه همکاری</LiTitle>
               <Li>افتتاح فروشگاه انلاین</Li>
               <Li>مشارکت در فروش</Li>
             </Ul>
+            </Tlist>
           </LinkWrapper>
         </TopSection>
-
+      
         <BottomSection container flexDirection={'column'} alignItems={'center'} justifyContent={'flex-start'}>
             <Socials container  alignItems={'center'} justifyContent={'space-evenly'}>
                 <Social><i class="bi bi-twitter social_icon"></i></Social>
@@ -71,7 +78,12 @@ const Footer = () => {
                 <Social><i class="bi bi-telegram social_icon"></i></Social>
                 <Social><i class="bi bi-linkedin social_icon"></i></Social>
             </Socials>
+            <Hr />
         </BottomSection>
+        
+        <InformationSection>
+
+        </InformationSection>
       </Wrapper>
     </>
   );
@@ -81,8 +93,11 @@ export default Footer;
 
 const Wrapper = styled(Grid)`
   background: rgba(0, 0, 0, 0.02);
-  height: 500px;
+  height: 700px;
   margin-top: 20px;
+  @media screen and (max-width : 600px) {
+    height : 900px;
+  }
 `;
 const BottomSection = styled(Grid)``;
 const TopSection = styled(Grid)`
@@ -116,6 +131,9 @@ const Ul = styled.ul`
   align-items: center;
   justify-content: space-evenly;
   flex-direction: column;
+  @media screen and (max-width : 600px) {
+    align-items : flex-start;
+  }
 `;
 const Li = styled.li`
   color: #555;
@@ -138,3 +156,16 @@ transition : all .3s ease;
     transform: scale(1.3);
 }
 `;
+
+const Flist = styled(Grid)``
+const Slist = styled(Grid)``
+const Tlist = styled(Grid)``
+
+const Hr = styled.hr`
+width : 96%;
+border: 2px solid #fff;
+margin-top: 30px;
+border-radius : 5px;
+`
+
+const InformationSection = styled(Grid)``

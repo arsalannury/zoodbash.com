d52337e947d0460849a401a3bfc21eb26dd9ba80
arsalannury <arsalannuryy@gmail.com>
2022-05-26 01:43:03 +0430

    styles for selers list

--- src/components/CategoryPage/EachProduct/Selers/Selers.js
+++ src/components/CategoryPage/EachProduct/Selers/Selers.js
@@ -1,10 +1,10 @@
-import { Div, SelerBox, Detail, Shop } from "./SelersStyle";
+import { Div, SelerBox, Title, Shop, Delivery } from "./SelersStyle";
 import React from "react";
 import { Grid, Rating, Button } from "@mui/material";
 import { useEffect, useState } from "react";
 import { createClient } from "@supabase/supabase-js";
 import shop from "../../../../images/shop.png";
-import AddShoppingCartOutlinedIcon from '@mui/icons-material/AddShoppingCartOutlined';
+import AddShoppingCartOutlinedIcon from "@mui/icons-material/AddShoppingCartOutlined";
 
 const Selers = () => {
   const [selersList, getSelersList] = useState([]);
@@ -27,33 +27,62 @@ const Selers = () => {
   }, []);
   return (
     <>
-      {selersList.map((seler) => (
-        <Div p={5} key={seler.id}>
-          <SelerBox
-            container
-            alignItems={"center"}
-            justifyContent={"space-around"}
+      <Grid m={6}>
+        {selersList.map((seler) => (
+          <Div p={1} m={2} key={seler.id}
+          // item
+          // sx={12}
+          // sm={12}
+          // md={6}
+          // lg={6}
+          // xl={6}
           >
-            <Shop src={shop} />
-            <Grid>
-              <Detail>{seler.seler}</Detail>
-              <Detail>{seler.delivery}</Detail>
-            </Grid>
-            <Grid>
-              <Detail>{seler.status}</Detail>
-              <Rating name="read-only" value={seler.score} readOnly />
-            </Grid>
-            <Button 
-            startIcon={<AddShoppingCartOutlinedIcon />}
-            size={'small'}
-            sx={{ marginTop: "0", "& .MuiButton-startIcon" : {margin:'5px'} }} 
-            variant="contained" 
-            color="success">
-              افزودن به سبد خرید
-            </Button>
-          </SelerBox>
-        </Div>
-      ))}
+            <SelerBox
+              container
+              alignItems={"center"}
+              justifyContent={"space-between"}
+            >
+              <Grid 
+              container
+              alignItems={'flex-start'}
+              justifyContent={'flex-start'}
+              item
+              sx={12}
+              sm={12}
+              md={6}
+              lg={6}
+              xl={6}
+              >
+              <Shop src={shop} />
+                <Grid mr={2}>
+                  <Title>{seler.seler}</Title>
+                  <Delivery>{seler.delivery}</Delivery>
+                </Grid>
+              </Grid>
+
+              {/* <Grid> */}
+                <Grid>
+                  <Title>{seler.status}</Title>
+                  <Rating name="read-only" value={seler.score} readOnly />
+                </Grid>
+              {/* </Grid> */}
+
+              <Button
+                startIcon={<AddShoppingCartOutlinedIcon />}
+                size={"small"}
+                sx={{
+                  marginTop: "0",
+                  "& .MuiButton-startIcon": { margin: "5px" },
+                }}
+                variant="contained"
+                color="success"
+              >
+                افزودن به سبد خرید
+              </Button>
+            </SelerBox>
+          </Div>
+        ))}
+      </Grid>
     </>
   );
 };
--- src/components/CategoryPage/EachProduct/Selers/SelersStyle.js
+++ src/components/CategoryPage/EachProduct/Selers/SelersStyle.js
@@ -1,12 +1,16 @@
-import styled from 'styled-components';
-import {Grid} from '@mui/material';
+import styled from "styled-components";
+import { Grid } from "@mui/material";
 
 export const Div = styled(Grid)`
+  border-radius: 5px;
+  box-shadow: 0 1px 2px #333;
+`;
+export const SelerBox = styled(Grid)``;
 
+export const Title = styled.p`
+color:#333;
+`;
+export const Shop = styled.img``;
+export const Delivery = styled.p`
+font-size : .7em;
 `
-export const SelerBox = styled(Grid)`
-
-`
-
-export const Detail = styled.p``
-export const Shop = styled.img``
